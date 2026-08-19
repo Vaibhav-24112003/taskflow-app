6a095848182f3ea434088761a44feb914411f827
Vaibhav-24112003 <bhoitev24@gmail.com>
2026-08-19 23:04:47 +0530

    ui: load Home dark theme overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import HomeSkin from './HomeSkin.jsx'
 import './home-glass.css'
+import './home-dark.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
